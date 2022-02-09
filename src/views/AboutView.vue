56b06c2f60cf759028fb5283bafd1f128f7ea1ab
Giang Pham <giangpham9500@gmail.com>
2022-02-09 12:22:19 -0600

    feat: integrate vue-router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import { useCounterStore } from '@/stores/counter'
+
+// const counter = useCounterStore()
+</script>
+
+<template>
+  <div>
+    <h2>This is About Page.</h2>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <img alt="Vue logo" src="@/assets/logo.png" class="logo" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + WindiCSS + Jest + Playwright" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  margin: auto;
+}
+</style>
