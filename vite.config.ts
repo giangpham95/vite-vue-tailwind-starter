3cbfc47115cfd2b73666ac67397f0b2a832e4257
Giang Pham <giangpham9500@gmail.com>
2022-02-09 11:22:40 -0600

    chore: update vite configuration and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  server: {
+    port: 8888,
+    open: false,
+    cors: true,
+    proxy: {},
+  },
 })
