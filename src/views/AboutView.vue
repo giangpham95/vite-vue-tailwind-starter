f550b621420f245b144f79d4190135dffc76870e
Giang Pham <giangpham9500@gmail.com>
2022-02-09 12:31:32 -0600

    feat: integrate pinia store

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-// import { useCounterStore } from '@/stores/counter'
+import { useCounter } from '@/store/counter'
 
-// const counter = useCounterStore()
+const counter = useCounter()
 </script>
 
 <template>
   <div>
     <h2>This is About Page.</h2>
+    <div>Count with pinia is: {{ counter.$state.count }}</div>
+    <button @click="counter.increment()">Click to Increase Count</button>
+    <button @click="counter.decrement()">Click to Decrease Count</button>
   </div>
 </template>
 
